e453bf5cf13e6deb6493d00c5ee83f6d1869c621
Abdelaziz18003 <m.abdelaziz1993@gmail.com>
2016-08-14 13:55:10 +0100

    apply a sliding effect on the navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,20 @@
-$("#share").jsSocials({
-    showLabel: false,
-    showCount: false,
-    shares: ["email", "twitter", "facebook", "googleplus", "linkedin", "pinterest", "stumbleupon", "whatsapp"]
-});
+
+$(document).ready(function(){
+    $("#share").jsSocials({
+        showLabel: false,
+        showCount: false,
+        shares: ["email", "twitter", "facebook", "googleplus", "linkedin", "pinterest", "stumbleupon", 
+                 "whatsapp"]
+    });
+
+    // modify navbar when scrolling
+    $(window).on("scroll", function(ev){
+        if(window.scrollY > 100){
+            $(".navbar").addClass("sticky");
+            console.log(window.scrollY);
+        } else if (window.scrollY) {
+            $(".navbar").removeClass("sticky");
+        }
+    })
+
+})
